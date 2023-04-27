dff1cf62539a8761ea1ff41492f8fca425a1f01a
Lucky23 <111248859+ItsLucky23@users.noreply.github.com>
2023-04-27 14:37:36 +0200

    Added logs

--- qbcore_admincage/server/server.lua
+++ qbcore_admincage/server/server.lua
@@ -1,39 +1,45 @@
+webhook = '' -- your discord webhook HERE
+playernames = '' -- dont change
+playerids = '' -- dont change
+
 RegisterNetEvent('admincage:handler')
 AddEventHandler('admincage:handler', function(args)
 
     local _source = source
-
     local xPlayer = QBCore.Functions.GetPlayer(_source)
         
-    if xPlayer.Functions.GetPermission() == 'admin' or xPlayer.Functions.GetPermission() == 'superadmin' then
-    --if xPlayer.Functions.GetPermission() == 'admin' or xPlayer.Functions.GetPermission() == 'superadmin' or xPlayer.Functions.GetPermission() == 'moderator' then
+    if xPlayer.Functions.GetPermission() ~= 'admin' and xPlayer.Functions.GetPermission() ~= 'superadmin' then
+    --if xPlayer.Functions.GetPermission() ~= 'admin' or xPlayer.Functions.GetPermission() ~= 'superadmin' or xPlayer.Functions.GetPermission() ~= 'moderator' then
+        return
+    end
 
-        if args[1] == 'true' then
+    if args[1] == 'true' then
+        SetPlayerRoutingBucket(_source, 1)
 
-            SetPlayerRoutingBucket(_source, 1)
+    elseif args[1] == 'false' then
+        SetPlayerRoutingBucket(_source, 0)
 
-        elseif args[1] == 'false' then
+    end
 
-            SetPlayerRoutingBucket(_source, 0)
+    if #args > 0 then
 
-        end
+        for i = 1, #args do
 
-        if #args > 0 then
+            if i ~= 1 then
 
-            for i = 1, #args do
-
-                if i ~= 1 then
+                if #GetPlayerIdentifiers(args[i]) > 0 then
+                    
+                    playernames = playernames..GetPlayerName(args[i])..' '
+                    playerids = playerids..args[i]..' '
 
                     if GetPlayerRoutingBucket(args[i]) ~= 0 then
-
                         SetPlayerRoutingBucket(args[i], 0)
 
                     else
-
                         SetPlayerRoutingBucket(args[i], 1)
 
                     end
-
+                    
                 end
 
             end
@@ -41,5 +47,13 @@ AddEventHandler('admincage:handler', function(args)
         end
 
     end
+
+    if #args > 1 then
+        message = GetPlayerName(source).. ' Admincaged ' ..playernames.. 'id(s): '..playerids
+        PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({content = message}), { ['Content-Type'] = 'application/json' })
+    end
+
+    playernames = '' -- dont change
+    playerids = '' -- dont change
 
 end)
